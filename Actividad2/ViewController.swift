bf767c9df7dff02a8323c312e1a62d19ab8257ff
Jair <jairantonio21@hotmail.com>
2022-09-08 21:08:06 -0500

    Initial Commit

---
+++ Actividad2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Actividad2
+//
+//  Created by Alumno on 08/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
